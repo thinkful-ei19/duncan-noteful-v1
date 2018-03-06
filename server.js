84ead70aa2e3b5ba7b5ac00c750247efd102eff3
trentduncan <trent.dunc@gmail.com>
2018-03-06 12:52:32 -0800

    finished error handlers

--- server.js
+++ server.js
@@ -4,9 +4,17 @@
 
 const express = require('express');
 const data = require('./db/notes');
+const {PORT} = require('./config');
 
 const app = express();
 app.use(express.static('public'));
+
+function requestLogger(req, res, next){
+  const now = new Date();
+  console.log(`${now.toLocaleDateString()} ${now.toLocaleTimeString()} ${req.method} ${req.url}`);
+  next();
+}
+app.use(requestLogger);
 
 app.get('/api/notes', (req, res) => {
   const {searchTerm} = req.query;
@@ -24,7 +32,26 @@ app.get('/api/notes/:id', (req, res) => {
   res.json(newData);
 });
 
-app.listen(8080, function () {
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+
+app.use(function(req, res, next){
+  let err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({message: 'Not Found'});
+});
+
+app.use(function(err, req, res, next){
+  res.status(err.status || 500);
+  res.json({
+    message:err.message,
+    error: err
+  });
+});
+
+
+app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
